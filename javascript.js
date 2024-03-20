267f7181ed23fd892e5cbc9e7f7879696cabc12e
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-03-20 18:16:00 -0400

    feat: Finalized project

--- javascript.js
+++ javascript.js
@@ -1,8 +1,56 @@
 let grid = document.querySelector(".grid");
+let body = document.querySelector("body");
+let form = document.querySelector("#form");
 const myLibrary = [];
+
+let newTitle = document.querySelector("#newtitle");
+let newAuthor = document.querySelector("#newauthor");
+let newPages = document.querySelector("#newpages");
+let ifUnread = document.querySelector("#unread");
         
+function clear() {
+    grid.textContent = '';
+}
+
+let add = document.querySelector(".add");
+add.addEventListener('click', addBook);
+
+let newButton = document.querySelector(".new");
+newButton.addEventListener('click', showForm);
+
+function showForm () {
+        form.style.opacity = 1;
+        newButton.removeEventListener('click', showForm);
+        newButton.addEventListener('click', hideForm);
+}
+
+function hideForm () {
+    form.style.opacity = 0;
+    newButton.removeEventListener('click', hideForm);
+    newButton.addEventListener('click', showForm);
+}
+
+function addBook() {
+    console.log('AYAYA');
+    let ifread = 'read';
+    if (ifUnread.checked) {
+        ifread = 'not read yet';
+    }
+    let temp = new Book(newTitle.value, newAuthor.value, newPages.value, ifread);
+    if (newTitle.value != '' && newAuthor.value != '' && newPages.value != '') {
+        addBookToLibrary(temp);
+        add.removeEventListener('click', addBook);
+        newTitle.value = '';
+        newAuthor.value = '';
+        newPages.value = '';
+        ifUnread.checked = true;
+        hideForm();
+        display();
+    }
+}
+
 function display() {
-    
+    clear();
     myLibrary.forEach(element => {
         let div =  document.createElement('div');
         let title = document.createElement('div');
@@ -17,6 +65,22 @@ function display() {
         div.appendChild(author);
         div.appendChild(pages);
         div.appendChild(read);
+        let button = document.createElement('button');
+        button.textContent = 'DELETE';
+        let secondbutton = document.createElement('button');
+        secondbutton.textContent = 'READ';
+        button.setAttribute('style', 'margin-bottom: -50px; margin-top: 30px;');
+        div.appendChild(secondbutton);
+        div.appendChild(button);
+        button.addEventListener('click', () => {grid.removeChild(div);})
+        secondbutton.addEventListener('click', () => {
+            if (read.textContent == 'read') {
+                read.textContent = 'not read yet';
+            }
+            else {
+                read.textContent = 'read';
+            }
+        })
         grid.appendChild(div);
     })
 }
@@ -46,7 +110,7 @@ addBookToLibrary(theHobbit);
 addBookToLibrary(theHobbit);
 addBookToLibrary(theHobbit);
 addBookToLibrary(theHobbit);
-addBookToLibrary(theHobbit);
+addBookToLibrary(new Book('AYAYA', 'J.R.R. Tolkien', 295, 'not read yet'));
 addBookToLibrary(theHobbit);
 addBookToLibrary(theHobbit);
 display();
